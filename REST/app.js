d1bb1bc40eadd1db856039edc1ad9f0c271b336a
Fuad Mohamoud <fuad.mohamoud@und.edu>
2017-10-21 00:10:39 -0500

    MongoDB And GET
    Made Get Request along with mongoDB. Created new Routes. Trying to validate GET request based off database information.

--- REST/app.js
+++ REST/app.js
@@ -1,10 +1,34 @@
-var express= require('express');
+var express= require('express'),
+mongoose = require('mongoose');
 
+//connection string to our database
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+
+var Book = require('./models/bookModel');
 //Create an instance of express
 var app= express();
 
 // Sets up a port, if our environment doesnt have one, we use port 3000
 var port = process.env.PORT || 3000;
+
+
+var bookRouter = express.Router();
+
+//Set up routers
+//req is the request coming in from the client, res is the response were gonna send back
+bookRouter.route('/Books')
+  .get(function(req,res){
+    Book.find(function(err,books){
+      if(err)
+        console.log(err)
+      else
+        res.json(books)
+    });
+  })
+
+app.use('/api',bookRouter);
+
 
 //Setting up our handler
 //First paramter is a slash which is the root fo our site
